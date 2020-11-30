832367a46440ab28442860257ab754d3d086d67e
Lillian Eng <lillian.eng88@gmail.com>
2020-11-29 17:53:51 -0800

    fixed sizing issue for proxy

--- client/components/gallery/PhotoGrid.jsx
+++ client/components/gallery/PhotoGrid.jsx
@@ -23,14 +23,14 @@ const PhotoGrid = ({photos, toggleCarousel, addPlace}) => {
       let lastRow = len - currentPhoto;
       if (randRow === 1 || lastRow === 1) {
         finalGrid.push(
-          <div key={currentPhoto + 1} className={styles.oneColumns}>
+          <div key={currentPhoto + 1}>
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 1} key={currentPhoto + 1} src={photos[currentPhoto]} width="100%"></img>
           </div>
         );
         currentPhoto += 1;
       } else if (randRow === 2 || lastRow === 2) {
         finalGrid.push(
-          <div key={currentPhoto + 1} className ={styles.twoColumns}>
+          <div key={currentPhoto + 1} >
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 1} key={currentPhoto + 1} src={photos[currentPhoto]} width="50%" height="70%"></img>
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 2} key={currentPhoto + 2} src={photos[currentPhoto + 1]} width="50%" height="70%"></img>
           </div>
@@ -38,7 +38,7 @@ const PhotoGrid = ({photos, toggleCarousel, addPlace}) => {
         currentPhoto += 2;
       } else if (randRow === 3 || lastRow === 3) {
         finalGrid.push(
-          <div key={currentPhoto + 1} className ={styles.threeColumns}>
+          <div key={currentPhoto + 1} >
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 1} key={currentPhoto + 1} src={photos[currentPhoto]} width="33.33%" height="45%"></img>
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 2} key={currentPhoto + 2} src={photos[currentPhoto + 1]} width="33.33%" height="45%"></img>
             <img onClick={() => { check(); }} className ={styles.photo} id={currentPhoto + 3} key={currentPhoto + 3} src={photos[currentPhoto + 2]} width="33.33%" height="45%"></img>
